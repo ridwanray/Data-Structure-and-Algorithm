7adac9dfc01221d20c0188a4b31193b137cb90a3
ridwanray <alabarise@gmail.com>
2022-01-31 18:49:49 +0100

    Recursive binart search

---
+++ recursive_binary_search.py
@@ -0,0 +1,22 @@
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = (len(list)) // 2
+        if list[midpoint] == target:
+            return True
+        else:
+            if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint+1:], target)
+            else:
+                return recursive_binary_search(list[:midpoint], target)
+
+
+def verify(result):
+    print('Target found:', result)
+
+
+numbers = range(0, 9999999999)
+
+result = recursive_binary_search(numbers, 9999999996)
+verify(result)
