de181fce40343153e3e187ffaa5150546ab78205
ridwanray <alabarise@gmail.com>
2022-02-01 04:53:10 +0100

    Access and Search Array

---
+++ arrays.py
@@ -0,0 +1,17 @@
+new_list = [1, 2, 3, 4, 5]
+result = new_list[6]
+
+# Access is a contant time operation on an array
+# or python list bcos the array stores references to
+# each of those object
+
+
+# 2. Searching an item in an array
+if 1 in new_list:
+    print(True)
+
+for n in new_list:
+    if n == 1:
+        print(True)
+
+        break
